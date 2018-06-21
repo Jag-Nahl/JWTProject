2de747ffc5274f435b422c6b72c8294577b33524
Jag Singh <jag@infojam.co.uk>
2018-06-21 14:28:27 +0100

    authorise

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using JWT.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace JWT.Controllers
 {
@@ -14,7 +15,8 @@ namespace JWT.Controllers
         {
             return View();
         }
-
+        
+        [Authorize]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
